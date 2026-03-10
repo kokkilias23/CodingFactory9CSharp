418b9fbade79b52bd877d0ec50dc7c35b13b1183
a8anassis <a8anassis@gmail.com>
2026-03-10 19:08:54 +0200

    chore: add FormatException example

---
+++ FomratExceptionApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FormatExceptionApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            while (true) 
+            {
+                try
+                {
+                    Console.WriteLine("Εισάγατε ένα ακέραιο (0 for Quit)");
+                    num = int.Parse(Console.ReadLine()!);
+                    Console.WriteLine($"Εισάγατε τον ακέραιο: {num}");
+                    if (num == 0) break;
+                }
+                catch (FormatException e) 
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
