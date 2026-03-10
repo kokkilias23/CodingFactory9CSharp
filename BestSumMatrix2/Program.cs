f8a27f2d7db29116a2d89f2d3e8f90bc4aecea66
Iliaskokkal <kokkiliasgrandera@gmail.com>
2026-03-10 21:25:16 +0200

    created PascalTriangle

---
+++ BestSumMatrix2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class MaxSubmatrixSum
+{
+    static void Main()
+    {
+        int[,] matrix = {
+            { 2, 5, 7, 7, 2 },
+            { 4, 1, 3, 6, 5 },
+            { 2, 8, 7, 8, 8 },
+            { 0, 4, 3, 2, 4 },
+            { 2, 1, 1, 9, 8 }
+        };
+
+        int rows = matrix.GetLength(0); // 5
+        int cols = matrix.GetLength(1); // 5
+        int subRows = 2; // ύψος υποπίνακα
+        int subCols = 3; // πλάτος υποπίνακα
+
+        // Prefix sum 2D για O(1) υπολογισμό κάθε υποπίνακα
+        int[,] prefix = new int[rows + 1, cols + 1];
+
+        for (int i = 1; i <= rows; i++)
+            for (int j = 1; j <= cols; j++)
+                prefix[i, j] = matrix[i - 1, j - 1]
+                              + prefix[i - 1, j]
+                              + prefix[i, j - 1]
+                              - prefix[i - 1, j - 1];
+
+        int maxSum = int.MinValue;
+        int maxRow = 0, maxCol = 0;
+
+        for (int i = subRows; i <= rows; i++)
+        {
+            for (int j = subCols; j <= cols; j++)
+            {
+                // Άθροισμα υποπίνακα που τελειώνει στο (i, j)
+                int sum = prefix[i, j]
+                        - prefix[i - subRows, j]
+                        - prefix[i, j - subCols]
+                        + prefix[i - subRows, j - subCols];
+
+                Console.WriteLine($"Υποπίνακας [{i - subRows},{j - subCols}] -> [{i - 1},{j - 1}]: sum = {sum}");
+
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                    maxRow = i - subRows;
+                    maxCol = j - subCols;
+                }
+            }
+        }
+
+        Console.WriteLine($"\n✅ Max Sum = {maxSum}");
+        Console.WriteLine($"📍 Θέση: γραμμή {maxRow}, στήλη {maxCol}");
+    }
+}
---
+++ PascalTriangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class PascalTriangle
+{
+    static void Main()
+    {
+        Console.Write("Δώσε ύψος τριγώνου: ");
+        int height = int.Parse(Console.ReadLine());
+
+        long[,] dp = new long[height, height];
+
+        for (int i = 0; i < height; i++)
+        {
+            dp[i, 0] = 1;
+            for (int j = 1; j <= i; j++)
+                dp[i, j] = dp[i - 1, j - 1] + dp[i - 1, j];
+        }
+
+        // Εκτύπωση
+        for (int i = 0; i < height; i++)
+        {
+            Console.Write(new string('*', (height - i - 1) * 2));
+            for (int j = 0; j <= i; j++)
+                Console.Write($" {dp[i, j],4} ");
+            Console.Write(new string('*', (height - i - 1) * 2));
+            Console.WriteLine();
+        }
+    }
+}
