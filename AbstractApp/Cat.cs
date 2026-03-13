fd0d35dee1876273dbb823d83345c400412bf2d2
a8anassis <a8anassis@gmail.com>
2026-03-13 20:58:08 +0200

    chore: add abstract class logic

---
+++ AbstractApp/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractApp
+{
+    internal class Cat : AbstractAnimal
+    {
+        public override void Eat()
+        {
+            base.Eat();
+            Console.WriteLine($"{Name} is finally eating.");
+        }
+        public override string ToString() => $"Id={Id}, Name={Name}, Age={Age}";
+    }
+}
